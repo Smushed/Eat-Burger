7f069a6c062808a23d7f3d4b3de7317217b84f92
Smushed <smushedcode@gmail.com>
2018-12-08 20:09:56 -0600

    Have the post route working, just need to work on the put route

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,26 @@
 $(() => {
     //Has to be a function because of using this keyword
     $(`.eat`).on(`click`, function (event) {
-        const id = $(this)
+        const id = $(this).data(`id`);
+        const userEat = {
+            //1 is the same thing as true in mySQL. Setting the value as true for devoured
+            devoured: 1
+        };
+
+        //TODO Make the put route & AJAX call
+        $.ajax(`/api/eat`)
+    });
+    $(`.submit`).on(`click`, function (event) {
+        const newBurger = {
+            //Takes the textarea on the html
+            burger_name: $(`#burger-input`).val().trim(),
+        };
+        $.ajax(`/api/burgers`, {
+            type: `POST`,
+            data: newBurger
+        }).then(() => {
+            location.reload();
+        }
+        );
     });
 })
